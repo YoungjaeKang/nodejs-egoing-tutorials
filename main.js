fad7ec222107e343527251c388e730f936165d50
Youngjae Kang <kyj8028@hotmail.com>
2021-04-18 17:47:51 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+var http = require('http');
+var fs = require('fs');
+var app = http.createServer(function(request,response){
+    var url = request.url;
+    if(request.url == '/'){
+      url = '/index.html';
+    }
+    if(request.url == '/favicon.ico'){
+        response.writeHead(404);
+        response.end();
+        return;
+    }
+    response.writeHead(200);
+    console.log(__dirname + url);
+    response.end(fs.readFileSync(__dirname + url)); // end()안에 있는 것들이 렌더링되어서 나온다.
+ 
+});
+app.listen(3000);
