e8958fa3980bd79efc824313bcc13af637b55df8
Youngjae Kang <kyj8028@hotmail.com>
2021-04-20 23:49:00 +0900

    complete!

--- main.js
+++ main.js
@@ -2,38 +2,12 @@ var http = require("http");
 var fs = require("fs");
 var url = require("url");
 var qs = require("querystring");
+var template = require('./lib/template.js');
+var path = require('path'); // 이걸로 하지 않으면 readfile이 있는 곳에서 외부에서 경로를 탐색해서 내 폴더를 볼 수 있음
+var sanitizeHtml = require('sanitize-html');
 
-function templateHTML(title, list, body) {
-  return `
-  <!doctype html>
-  <html>
-  <head>
-    <title>WEB1 - ${title}</title>
-    <meta charset="utf-8">
-  </head>
-  <body>
-    <h1><a href="/">WEB</a></h1>
-    ${list}
-    <a href="/create">create</a>
-    ${body}
-  </body>
-  </html>
-  `;
-}
-
-function templateList(filelist) {
-  var list = "<ul>";
-  var i = 0;
-  while (i < filelist.length) {
-    list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-    i = i + 1;
-  }
-  list = list + "</ul>";
-  return list;
-}
-
-var app = http.createServer(function (request, response) {
-  var _url = request.url;
+  var app = http.createServer(function (request, response) {
+    var _url = request.url;
   var queryData = url.parse(_url, true).query;
   var pathname = url.parse(_url, true).pathname;
   // console.log(pathname);
@@ -43,29 +17,40 @@ var app = http.createServer(function (request, response) {
         // console.log(filelist);
         var title = "Welcome!!";
         var description = "Hello, Node.js";
-        var list = templateList(filelist);
-        var body = `<h2>${title}</h2><p>${description}</p>`;
-        var template = templateHTML(title, list, body);
+        var list = template.list(filelist);
+        var html = template.HTML(title, list, `<h2>${title}</h2><p>${description}</p>`, `<a href="/create?id=${title}">create</a>`);
 
         response.writeHead(200);
-        response.end(template);
+        response.end(html);
       });
 
       // response.end(fs.readFileSync(__dirname + _url)); // end()안에 있는 것들이 렌더링되어서 나온다.
     } else {
       fs.readdir("./data", function (error, filelist) {
-        var list = templateList(filelist);
-
+        var filteredId = path.parse(queryData.id).base;
         fs.readFile(
-          `data/${queryData.id}`,
+          `data/${filteredId}`,
           "utf8",
           function (err, description) {
             var title = queryData.id;
-            var body = `<h2>${title}</h2><p>${description}</p>`;
-            var template = templateHTML(title, list, body);
+            var sanitizedTitle = sanitizeHtml(title);
+            var sanitizedDescription = sanitizeHtml(description, {
+              allowedTags:['h1']
+            });
+            var list = template.list(filelist);
+            var html = template.HTML(sanitizedTitle, list, `<h2>${sanitizedTitle}</h2><p>${sanitizedDescription}</p>`,
+            `<a href="/create">create</a>
+            <a href="/update?id=${sanitizedTitle}">update</a>
+            <form action="delete_process" method="post" onsubmit="javascript">
+              <input type="hidden" name="id" value="${sanitizedTitle}">
+              <input type="submit" value="delete">
+            </form>`);
+            // delete는 a태그로 구현하는 GET방식으로 구현하면 안되고 POST로 해야 함
+
+
 
             response.writeHead(200);
-            response.end(template);
+            response.end(html);
             // response.end(fs.readFileSync(__dirname + _url)); // end()안에 있는 것들이 렌더링되어서 나온다.
           }
         );
@@ -76,9 +61,9 @@ var app = http.createServer(function (request, response) {
       // console.log(filelist);
       var title = "WEB - create";
 
-      var list = templateList(filelist);
+      var list = template.list(filelist);
       var body = `
-      <form action="http://localhost:3000/create_process" method="post">
+      <form action="/create_process" method="post">
         <p><input type="text" name="title" placeholder="title"></p>
         <p>
           <textarea name="description" placeholder="decsription"></textarea></textarea>
@@ -88,10 +73,10 @@ var app = http.createServer(function (request, response) {
         </p>
       </form>
       `;
-      var template = templateHTML(title, list, body);
+      var html = template.HTML(title, list, body, '');
 
       response.writeHead(200);
-      response.end(template);
+      response.end(html);
     });
 
   } else if (pathname === '/create_process') {
@@ -113,7 +98,74 @@ var app = http.createServer(function (request, response) {
         response.end("success");
       })
     });
+  } else if (pathname === '/update') {
+    fs.readdir("./data", function (error, filelist) {
+      var filteredId = path.parse(queryData.id).base;
+      fs.readFile(
+        `data/${filteredId}`,
+        "utf8",
+        function (err, description) {
+          var title = queryData.id;
+          var list = template.list(filelist);
+          var html = template.HTML(title, list, `
+          <form action="/update_process" method="post">
+            <input type="hidden" name="id" value="${title}">
+            <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+            <p>
+              <textarea name="description" placeholder="decsription">${description}</textarea>
+            </p>
+            <p>
+              <input type="submit">
+            </p>
+          </form>
+          `,
+          `<a href="/create">create</a> <a href="/update?=${title}">update</a>`);
+          
 
+          response.writeHead(200);
+          response.end(html);
+          // response.end(fs.readFileSync(__dirname + _url)); // end()안에 있는 것들이 렌더링되어서 나온다.
+        }
+      );
+    });
+  } else if (pathname === '/update_process') {
+    var body = '';
+    request.on('data', function(data) {
+      body += data;
+      // post 방식으로 전송되는 데이터가 많을 경우를 대비
+      // if (body.length > 1e6)
+      //   request.connection.destroy();
+    });
+    request.on('end', function(){
+      var post = qs.parse(body);
+      var id = post.id;
+      var title = post.title;
+      var description = post.description;
+      fs.rename(`data/${id}`, `data/${title}`, function(error){
+        fs.writeFile(`data/${title}`, description, 'utf8', function(err) {
+            response.writeHead(302, {Location: `/?id=${title}`});
+            response.end("success");
+          })
+          console.log(post);
+        })
+    });
+  } else if (pathname === '/delete_process') {
+    var body = '';
+    request.on('data', function(data) {
+      body += data;
+      // post 방식으로 전송되는 데이터가 많을 경우를 대비
+      // if (body.length > 1e6)
+      //   request.connection.destroy();
+    });
+    request.on('end', function(){
+      var post = qs.parse(body);
+      var id = post.id;
+      var filteredId = path.parse(id).base;
+      fs.unlink(`data/${filteredId}`, function(error) {
+        response.writeHead(302, {Location: `/`});
+        response.end();
+      })
+    });
   } else {
     response.writeHead(404);
     response.end("Not Found");
